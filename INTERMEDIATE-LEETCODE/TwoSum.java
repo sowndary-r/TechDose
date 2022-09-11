775a298a31678f11e539e482f5c962d6c3771171
SOWNDARYA R <107302244+sowndary-r@users.noreply.github.com>
2022-09-11 19:35:23 +0530

    Update TwoSum.java

--- INTERMEDIATE-LEETCODE/TwoSum.java
+++ INTERMEDIATE-LEETCODE/TwoSum.java
@@ -21,6 +21,7 @@ Constraints:
 -109 <= target <= 109
 Only one valid answer exists.*/
 
+/*Solution Approach - bruteforce*/
 class Solution {
     public int[] twoSum(int[] nums, int target) { 
       int idx[]=new int[2];
