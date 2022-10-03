a96fe68068c4059cdcbffc0ab71e5e1c9b4cfae8
SOWNDARYA R <107302244+sowndary-r@users.noreply.github.com>
2022-10-03 12:11:51 +0530

    Create Find all anagrams in a string.java

---
+++ ARRAYS ALGORITHM/SLIDING WINDOW/Find all anagrams in a string.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+     int i=0,idx=0;
+     int n=s.length();
+     int k=p.length(); 
+   char c[]=p.toCharArray(); 
+     Arrays.sort(c);
+    List<Integer> l=new ArrayList<>(); 
+  for(i=0;i<=n-k;i++)
+  { 
+   String sub=s.substring(i,i+k);
+   char ch[]=sub.toCharArray();
+   Arrays.sort(ch);
+   if(Arrays.equals(c,ch))
+   l.add(i);
+   return l;
+   
+    }
+}
