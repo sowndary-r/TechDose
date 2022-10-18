b5263cd41accaab0d66b5d9c10b3077231fa042a
SOWNDARYA R <107302244+sowndary-r@users.noreply.github.com>
2022-10-18 18:50:13 +0530

    Update SortLinkedList.java

--- LINKED LIST/SortLinkedList.java
+++ LINKED LIST/SortLinkedList.java
@@ -1,68 +1,72 @@
 
-class Solution {
-    public ListNode sortList(ListNode head) {
-         if(head == null || head.next == null)
-            return head;
-        
-        
-        ListNode temp = null;
-        ListNode slow = head;
-        ListNode fast = head;
-        
-        //middle of linked list
-        while(fast !=  null && fast.next != null){
-            temp = slow;
-            slow = slow.next;          
-            fast = fast.next.next;  
-            
-        }   
-        temp.next = null;            
-        
-        ListNode l1 = sortList(head);    
-        ListNode l2 = sortList(slow);  
-        
-        return mergelist(l1, l2);         
-            
-    }
-    
-    ListNode mergelist(ListNode head1, ListNode head2)
-    {
-        ListNode fh=null, ft=null;
-    while(head1!=null && head2!=null){
-        
-        if(fh== null && ft==null){
-            if(head1.val>head2.val){
-                fh=head2;
-                ft=head2;
-                head2=head2.next;
-            }else{
-                fh=head1;
-                ft=head1;
-                head1=head1.next;
-            }
-        }
-        if(head1!=null && head2!=null) {
-            if(head1.val<head2.val){
-                ft.next=head1;
-                ft=ft.next;
-                head1=head1.next;
 
-            }else{
-                ft.next=head2;
-                ft=ft.next;
-                head2=head2.next;
-            }
-        }
+class Solution {
+    public ListNode sortList(ListNode head) { 
+   if(head==null || head.next==null)
+       return head;
+      ListNode slow=head;
+      ListNode fast=head;
+      ListNode temp=null;
+while(fast!=null && fast.next!=null)
+    { 
+       temp=slow;
+       slow=slow.next;
+       fast=fast.next.next;
+    
         
     }
+    temp.next=null;
+  ListNode l1=sortList(head);
+  ListNode l2=sortList(slow);
+   return mergeSort(l1,l2);
     
-    if(head1!=null){
-        ft.next=head1;
+    } 
+ static ListNode mergeSort (ListNode h1,ListNode h2)
+     
+{ 
+ ListNode fh=null;
+ ListNode ft=null;
+   while(h1!=null &&h2!=null) 
+  { 
+    if(fh==null&&ft==null)
+    { 
+      if(h1.val<h2.val)
+      { 
+      fh=h1;
+      ft=h1;
+      h1=h1.next;
+          
+      }
+      else 
+      { 
+       fh=h2;
+       ft=h2;
+       h2=h2.next;
+      }
     }
-    if(head2!=null){
-        ft.next=head2;
+   else
+   { 
+    if(h1.val<h2.val)
+    { 
+     ft.next=h1; 
+     ft=ft.next;
+     h1=h1.next;
     }
+    else
+    { 
+     ft.next=h2;
+     ft=ft.next;
+     h2=h2.next;
+    }
+   }
     
-    return fh;
-    }
+  }
+  if(h1!=null)
+  ft.next=h1;
+  if(h2!=null)
+  ft.next=h2;
+  
+  return fh;     
+  
+}
 }
