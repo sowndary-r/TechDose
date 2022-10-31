ef496394d6537ff9d706b90ab2fe637b56d9207f
SOWNDARYA R <107302244+sowndary-r@users.noreply.github.com>
2022-10-31 10:19:19 +0530

    Create DesignHashMap.java

---
+++ HASHMAP/DesignHashMap.java
@@ -0,0 +1,29 @@
+class MyHashMap {
+     int arr[];
+    public MyHashMap() {
+        this.arr=new int[10000001];
+    }
+    
+    public void put(int key, int value) {
+        arr[key]=value+1;
+        
+    }
+    
+    public int get(int key) {
+        return arr[key]-1;
+        
+    }
+    
+    public void remove(int key) {
+        arr[key]=0;
+        
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
