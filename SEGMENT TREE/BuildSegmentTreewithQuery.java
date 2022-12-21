1b38a5b62728a114868b0bdeaba56ca0b79f160c
SOWNDARYA R <107302244+sowndary-r@users.noreply.github.com>
2022-12-21 15:14:22 +0530

    Update BuildSegmentTreewithQuery.java

--- SEGMENT TREE/BuildSegmentTreewithQuery.java
+++ SEGMENT TREE/BuildSegmentTreewithQuery.java
@@ -1,17 +1,21 @@
 public class Main
 {
     
-    static 	int a[]={21,6,15,4,2,10,5,1,3};
-    static  int seg[];
+    static int a[]={1,3,2,-2,4,5};
+    static int seg[];
 	public static void main(String[] args) {
-    int n=9;
+    int n=6;
     seg=new int[4*n+1];
 	buildSegmentTree(1,0,n-1);
 	for(int i:seg)
 	System.out.println(i);
 	System.out.println("QUERY_SUM(2,4) --> "+query(2,4,1,0,n-1));
+	update(1,0,n-1,2,100);
+	System.out.println("UPDATED QUERY ");
+	for(int i:seg)
+	System.out.println(i);
 	}
-	static void buildSegmentTree(int st,int start,int end)
+	static void buildSegmentTree(int st,int start,int end) //Build segment tree
 	{
 	    if(start>end)
 	    return;
@@ -25,7 +29,7 @@ public class Main
 	    buildSegmentTree(2*st+1,mid+1,end);
 	    seg[st]=seg[2*st]+seg[2*st+1];
 	}
-	static int query(int qs,int qe,int st,int start,int end)
+	static int query(int qs,int qe,int st,int start,int end) //Query function
 	{
 	  if(qs>end || qe<start)
 	  return 0;
@@ -36,5 +40,19 @@ public class Main
 	  int rytsum=query(qs,qe,2*st+1,mid+1,end);
 	  return leftsum+rytsum;
 	    
+	}
+	static void update(int st,int start,int end,int pos,int val) //update function
+	{
+	    if(start>pos || end<pos)
+	    return;
+	    if(start==end)
+	    {
+	       seg[st]=val;
+	       return;
+	    }
+	    int mid=start+(end-start)/2;
+	    update(2*st,start,mid,pos,val);
+	    update(2*st+1,mid+1,end,pos,val);
+	    seg[st]=seg[2*st]+seg[2*st+1];
 	}
 }
