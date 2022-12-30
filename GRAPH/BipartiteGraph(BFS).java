163ab48029633b92118280799fcbc42c78d5fd0a
SOWNDARYA R <107302244+sowndary-r@users.noreply.github.com>
2022-12-30 13:04:45 +0530

    Update BipartiteGraph(BFS).java

--- GRAPH/BipartiteGraph(BFS).java
+++ GRAPH/BipartiteGraph(BFS).java
@@ -3,13 +3,12 @@ import java.util.*;
 public class Main
 {
     //Bipartite graph checking using BFS
-    static boolean bipar(List<List<Integer>> adj,int v)
+    static boolean bipar(List<List<Integer>> adj,int[] color,int curr,int v)
     {
         Queue<Integer> q=new LinkedList<>();
-        int color[]=new int[v];
-	   	Arrays.fill(color,-1); 
-        q.add(0);
-        color[0]=0;
+        
+        q.add(curr);
+        color[curr]=0;
         while(!q.isEmpty())
         {
             int node=q.poll();
@@ -34,36 +33,36 @@ public class Main
 		adj.add(new ArrayList<>());
 		
 		//adjacency list
-		/*adj.get(0).add(1);
+	      /*adj.get(0).add(1);
 	  	adj.get(1).add(0);
-		  adj.get(1).add(2);
-		  adj.get(1).add(4);
-	    adj.get(2).add(1);
-	    adj.get(2).add(3);
-	    adj.get(3).add(2);
+		adj.get(1).add(2);
+		adj.get(1).add(4);
+	        adj.get(2).add(1);
+	        adj.get(2).add(3);
+	        adj.get(3).add(2);
 	   	adj.get(3).add(5); 
 	   	adj.get(3).add(6);   //not a bipartite graph
 	   	adj.get(4).add(1);
 	   	adj.get(4).add(5);
 	   	adj.get(5).add(3);
-	    adj.get(5).add(4);
+	        adj.get(5).add(4);
 	   	adj.get(6).add(3);
 	   	adj.get(6).add(7);
 	   	adj.get(7).add(6);*/
 	   
-	    adj.get(0).add(1);
-		  adj.get(1).add(0);
-		  adj.get(1).add(2);
-		  adj.get(2).add(3);
-	    adj.get(2).add(1);
-	    adj.get(2).add(5);
-	    adj.get(3).add(2);
+	        adj.get(0).add(1);
+		adj.get(1).add(0);
+		adj.get(1).add(2);
+		adj.get(2).add(3);
+	        adj.get(2).add(1);
+	        adj.get(2).add(5);
+	        adj.get(3).add(2);
 	   	adj.get(3).add(4);
 	   	adj.get(4).add(3);
 	   	adj.get(4).add(7);
 	   	adj.get(5).add(2);   //bipartite graph
 	   	adj.get(5).add(6);
-	    adj.get(6).add(5);
+	        adj.get(6).add(5);
 	   	adj.get(6).add(7);
 	   	adj.get(7).add(4);
 	   	adj.get(7).add(6);
@@ -71,10 +70,20 @@ public class Main
 	   	adj.get(8).add(7);
 	   	adj.get(8).add(9);
 	   	adj.get(9).add(8);
-	   
-	   	if(bipar(adj,v))
+	   	int color[]=new int[v];
+	   	Arrays.fill(color,-1); 
+	    for(int i=0;i<v;i++)
+	    {
+	        if(color[i]==-1)
+	        {
+	            if(bipar(adj,color,i,v)==false)
+	            {
+	           System.out.println("Not a Bipartite graph");
+	           return;
+	            }
+	        }
+	    }
 	   	System.out.println("Bipartite graph");
-	    else
-	   	System.out.println("not a bipartite graph");
+	
 	}
 }
